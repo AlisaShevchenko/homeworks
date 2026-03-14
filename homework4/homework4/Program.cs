3f0a4f9c53942d47776f8b8623c56c1ba39909c8
Alisa <alice.shevchenko95@gmail.com>
2026-03-14 12:25:03 +0200

    Added a solution to the problem

--- homework4/homework4/Program.cs
+++ homework4/homework4/Program.cs
@@ -1,1 +1,38 @@
 ﻿// ДЗ 4.1. Назва дня тижня
+
+Console.Write("Enter a number for the day of the week (1-7): ");
+string input = Console.ReadLine();
+if (int.TryParse(input, out int numberOfDays))
+{
+    switch (numberOfDays)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+        default:
+            Console.WriteLine("You entered an invalid number.");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a valid number.");
+}
