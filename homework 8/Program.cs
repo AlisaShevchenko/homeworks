6993e4a9752e1bf91ea6b6da5f66613c06e66ad3
Alisa <alice.shevchenko95@gmail.com>
2026-04-04 15:44:37 +0300

    Added homework 9.1, 9.2, 9.3

---
+++ homework 8/Program.cs
@@ -0,0 +1,63 @@
+﻿//Task 9.1
+
+Console.Write("Enter your name and surname: ");
+string input = Console.ReadLine();
+string[] array = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+if (array.Length < 2)
+{
+    Console.WriteLine("Please enter both name and surname");
+    return;
+}
+string name = array[0];
+string lastName = array[1];
+
+if(char.ToLower(name[0]) == char.ToLower(lastName[0]))
+{
+    Console.WriteLine("Last name starts with the same letter as first name");
+}
+else
+{
+    Console.WriteLine("The last name does not start with the same letter as the first name");
+}
+
+// Task 9.2
+
+
+using System.Dynamic;
+using System.Text;
+Console.WriteLine("Please enter the events");
+
+StringBuilder report = new StringBuilder();
+
+Console.WriteLine("---Report Generation---"); 
+Console.Write("Enter report title: ");
+string title = Console.ReadLine();
+    
+report.AppendLine("Title: " + title);
+Console.Write("Enter report date (dd.mm.yyyy): "); 
+string date = Console.ReadLine();
+report.AppendLine("Date: " + date);
+Console.WriteLine("Enter report event (type 'save' to finish): ");
+
+report.AppendLine("Events:");
+while (true)
+{
+    Console.Write("Event: ");
+    string events = Console.ReadLine();
+    if (events.ToLower() == "save")
+        break;
+    if (!string.IsNullOrWhiteSpace(events))
+    {
+        report.AppendLine("- " + events);
+    } 
+}
+Console.WriteLine();    
+Console.WriteLine("===Your finale report===");
+Console.Write(report.ToString());
+Console.WriteLine("------------------------");
+
+
+// Task 9.3
+string words = "apple, orange, blueberry, banana";
+string noSpaces = words.Replace(" ", "");
+Console.WriteLine($"Result: {noSpaces}");
