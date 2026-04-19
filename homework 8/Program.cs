e9e4019f9ec68550f9393193d86e387b4c7178cb
Alisa <alice.shevchenko95@gmail.com>
2026-04-19 15:17:30 +0300

    added homework 8.2

--- homework 8/Program.cs
+++ homework 8/Program.cs
@@ -1,4 +1,4 @@
-﻿//Task 9.1
+﻿/*//Task 9.1
 
 Console.Write("Enter your name and surname: ");
 string input = Console.ReadLine();
@@ -29,12 +29,12 @@ Console.WriteLine("Please enter the events");
 
 StringBuilder report = new StringBuilder();
 
-Console.WriteLine("---Report Generation---"); 
+Console.WriteLine("---Report Generation---");
 Console.Write("Enter report title: ");
 string title = Console.ReadLine();
-    
+
 report.AppendLine("Title: " + title);
-Console.Write("Enter report date (dd.mm.yyyy): "); 
+Console.Write("Enter report date (dd.mm.yyyy): ");
 string date = Console.ReadLine();
 report.AppendLine("Date: " + date);
 Console.WriteLine("Enter report event (type 'save' to finish): ");
@@ -49,9 +49,9 @@ while (true)
     if (!string.IsNullOrWhiteSpace(events))
     {
         report.AppendLine("- " + events);
-    } 
+    }
 }
-Console.WriteLine();    
+Console.WriteLine();
 Console.WriteLine("===Your finale report===");
 Console.Write(report.ToString());
 Console.WriteLine("------------------------");
@@ -60,4 +60,22 @@ Console.WriteLine("------------------------");
 // Task 9.3
 string words = "apple, orange, blueberry, banana";
 string noSpaces = words.Replace(" ", "");
-Console.WriteLine($"Result: {noSpaces}");
+Console.WriteLine($"Result: {noSpaces}");*/
+
+using System.Text;
+
+Console.Write("Enter your text: ");
+
+StringBuilder sb = new StringBuilder();
+
+string input = Console.ReadLine();
+string[] array = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length > 3)
+    {
+        sb.Append(array[i]);
+        sb.Append(" ");
+    }
+}
+Console.WriteLine(sb.ToString());
