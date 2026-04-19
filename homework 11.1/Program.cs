7627ae251c6573aa23ef57438957ecabd8da2a29
Alisa <alice.shevchenko95@gmail.com>
2026-04-19 16:56:36 +0300

    added homework 11.1

---
+++ homework 11.1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Enter input parametr");
+int input;
+while (!int.TryParse(Console.ReadLine(), out input) || input < 0)
+{
+    Console.WriteLine("The input must be a positive integer");
+    Console.WriteLine("Enter input parametr");
+}
+
+long number = FibonacciHelper.GetFibonacci(input);
+Console.WriteLine($"Output value: {number}");
+
+public static class FibonacciHelper
+{
+    public static long GetFibonacci(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        
+    }
+}
