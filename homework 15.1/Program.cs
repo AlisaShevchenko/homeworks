77bc312dc256fde63b040cc856bece122b1496ef
Alisa <alice.shevchenko95@gmail.com>
2026-05-02 13:12:47 +0300

    homework 15.1

---
+++ homework 15.1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введіть шлях до вихідного файлу: "); 
+string sourcePath = Console.ReadLine();
+Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: "); 
+string destinationPath =  Console.ReadLine();
+try
+{
+    if (File.Exists(sourcePath))
+    {
+        File.Copy(sourcePath, destinationPath, true);
+        Console.WriteLine("Файл успішно скопійовано!");
+    }
+    else
+    {
+        Console.WriteLine("Помилка: Вихідний файл не знайдено.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Помилка: {ex.Message}");
+}
+
