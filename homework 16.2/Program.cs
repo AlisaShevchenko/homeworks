b7321a77343eebe65aeadaf0bed4b14d350129e6
Alisa <alice.shevchenko95@gmail.com>
2026-05-03 13:10:29 +0300

    added homework 16.2

---
+++ homework 16.2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter the number: ");
+double number;
+while (true)
+{ 
+    try
+    {
+        string input = Console.ReadLine();
+        if (!double.TryParse(input, out number))
+        {
+            throw new NotANumberException("Not a number. Please try again.");
+        }
+        Console.WriteLine($"Success! Your number is {number}");
+        break;
+    }
+    catch (NotANumberException e)
+    {
+        Console.WriteLine($"Error: {e.Message}");
+        Console.Write("Enter the number: ");
+    }
+}
+
+public class NotANumberException : Exception
+{
+    public NotANumberException(string message) : base(message) { }
+}
